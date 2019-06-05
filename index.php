85c1d181a75be046df1bf83b9c7cf3a83d614947
NITISH PEEROO <nitish.peeroo@gmail.com>
2019-06-05 19:58:56 +0200

    tt

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
     require_once('vendor/autoload.php');
     $sender = "sixtrone@gmail.com";
-    $to = array('nitish.peeroo@gmail.com','nancy.test1@mmcp.groupe-efrei.fr','test2.toto@mmcp.groupe-efrei.fr');
+    $to = array('nancy.test1@mmcp.groupe-efrei.fr','test2.toto@mmcp.groupe-efrei.fr');
     // Create the Transport
-    $transport = (new Swift_SmtpTransport('smtp.gmail.com',587,'tls'))
+    $transport = (new Swift_SmtpTransport('smtp.gmail.com',465,'ssl'))
         ->setUsername($sender)
         ->setPassword('gf4mP321@gv5');
-
 // Create the Mailer using your created Transport
     $mailer = new Swift_Mailer($transport);
-
 // Create a message
     $message = (new Swift_Message('Wonderful Subject'))
         ->setFrom([$sender])
